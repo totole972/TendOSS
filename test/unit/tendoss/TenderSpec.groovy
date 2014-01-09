0b95994fcf75a98f595e9ddf4faa69390ec87245
Xavier Varlez <xvarlez@gmail.com>
2014-01-09 14:32:01 +0100

    #19 Wrote unit tests for Answer and Tender classes

--- test/unit/tendoss/TenderSpec.groovy
+++ test/unit/tendoss/TenderSpec.groovy
@@ -17,4 +17,35 @@ class TenderSpec extends Specification {
 
     void "test something"() {
     }
+	
+	def "Test constraints"() {
+		setup:
+		mockForConstraintsTests(Tender)
+
+		when:
+		def tender = new Tender(name: name, submissionDate: submissionDate, closed: closed)
+		tender.validate()
+
+		then:
+		tender.hasErrors() == !valid
+
+		where:
+		name 		| 	submissionDate	|	closed	|	valid
+		null		|	null			|	null	|	false
+		"A name" 	| 	null			|	null	|	false
+		null		| 	new Date()		|	null	|	false
+		null		| 	null			|	false	|	false
+		"A name"	| 	new Date()		|	null	|	false
+		"A name"	| 	null			|	false	|	false
+		null		| 	new Date()		|	false	|	false
+		""			|	""				|	""		|	false
+		"A name" 	| 	""				|	""		|	false
+		""			| 	new Date()		|	""		|	false
+		""			| 	""				|	false	|	false
+		"A name"	| 	new Date()		|	""		|	false
+		"A name"	| 	""				|	false	|	false
+		""			| 	new Date()		|	false	|	false
+	}
 }
+
+
