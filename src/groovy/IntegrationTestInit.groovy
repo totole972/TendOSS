10c1d812fe27bda58efe6fc437fa3d5de2ed808c
Kevin <kevanatole@gmail.com>
2014-02-09 16:12:59 +0100

    Initialisation des données de tests d'inté

---
+++ src/groovy/IntegrationTestInit.groovy
@@ -0,0 +1,28 @@
+/**
+ * Created by totole on 09/02/14.
+ */
+package tendoss.test
+class IntegrationTestInit {
+
+    static initData(){
+
+        //create users
+
+
+
+        //create tenders
+
+
+
+        //create answers
+
+
+        //create votes
+
+            //for users
+            //for tenders
+            //for answers
+
+    }
+
+}
