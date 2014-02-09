0d4a5e477bcf8433e179a9b04aa4eaa2c92d7bec
dam4256 <arondeldamien@orange.fr>
2014-02-09 18:27:04 +0100

    #3 New service to create a user account

--- grails-app/domain/tendoss/Role.groovy
+++ grails-app/domain/tendoss/Role.groovy
@@ -6,9 +6,25 @@ class Role {
 
     static mapping = {
         cache true
+		version(false)
     }
 
     static constraints = {
-        authority blank: false, unique: true
+        authority blank: false, unique: true, inList: RoleEnum.values()*.name()
     }
+	
+	enum RoleEnum {
+		ADMIN_ROLE(1),
+		USER_ROLE(2)
+	
+		Long id
+	
+		RoleEnum(Long id) {
+			this.id = id
+		}
+	
+		Role getRole() {
+			Role.get(id)
+		}
+	}
 }
--- grails-app/services/tendoss/UserService.groovy
+++ grails-app/services/tendoss/UserService.groovy
@@ -1,11 +1,21 @@
 package tendoss
 
-import grails.transaction.Transactional
-
-@Transactional
 class UserService {
 
-    def serviceMethod() {
-
-    }
+	/**
+	 * Adds a new user with a specific role.
+	 * 
+	 * @param user the user
+	 * @param role the role
+	 * @param enabled the flag indicating if the account is enabled
+	 * @return the created user
+	 */
+	User addUser(User user, Role role, boolean enabled = true) {
+		user.enabled = enabled
+		user.save()
+		if (!user.hasErrors()) {
+			UserRole.create(user, role)
+		}
+		user
+	}
 }
